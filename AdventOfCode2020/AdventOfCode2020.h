50c0ffa0186a6ab69d85d0aebe14d7c0c2f01713
Tim Searle <timothy.searle@mnscorp.net>
2020-12-02 22:26:25 +0000

    Initial Commit

---
+++ AdventOfCode2020/AdventOfCode2020.h
@@ -0,0 +1,18 @@
+//
+//  AdventOfCode2020.h
+//  AdventOfCode2020
+//
+//  Created by C3924521_Tim_Searle on 02/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdventOfCode2020.
+FOUNDATION_EXPORT double AdventOfCode2020VersionNumber;
+
+//! Project version string for AdventOfCode2020.
+FOUNDATION_EXPORT const unsigned char AdventOfCode2020VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdventOfCode2020/PublicHeader.h>
+
+
